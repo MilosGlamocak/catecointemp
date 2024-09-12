347e3199d9d607871e6d8b1da5a1faae2a8e098a
MilosGlamocak <milosglamocak@gmail.com>
2024-09-12 23:57:04 +0200

    a

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -9,13 +9,18 @@ function WelcomePage() {
   return (
     <div className='welcomePageCont'>
         <div className='logoDiv'>
-            <h1 className='logo'>BLUB</h1>
+            <img src="https://www.blubsui.com/dd.png" alt="logo" className='logo'/>
         </div>
         <div className='menuBtns'>
             {buttons.map((b) => {
                 return <MenuButton text={b} key={b}/>
             })}
         </div>
+        <div className='logoBig'>
+            <img src="https://www.blubsui.com/ccc.png" alt="logoBig" />
+            <h1>A Dirty Fish in the Waters of the Sui Ocean</h1>
+        </div>
+        <div className='welcomeContactDiv'></div>
     </div>
   )
 }
