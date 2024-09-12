e34ae4e601c79a82c61603a2fb5cd63ed09328b0
MilosGlamocak <milosglamocak@gmail.com>
2024-09-13 00:22:07 +0200

    a

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -22,10 +22,13 @@ function WelcomePage() {
             <img src="https://www.blubsui.com/ccc.png" alt="logoBig" />
             <h1>A Dirty Fish in the Waters of the Sui Ocean</h1>
         </div>
+        <img src="https://www.blubsui.com/bubbles.png" alt="bubbles" className='mainDecoration'/>
         <div className='welcomeContactDiv'>
             <div className='welcomeContactIcon'><XIcon /></div>
             <div className='welcomeContactIcon'><TelegramIcon /></div>
-            
+        </div>
+        <div className='leftDecoration'>
+            <img src="https://www.blubsui.com/tesouro.png" alt="decoration" />
         </div>
     </div>
   )
