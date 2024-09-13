41cd8d59a6653d8256f87ae3f33146c86ec4f41c
MilosGlamocak <milosglamocak@gmail.com>
2024-09-13 11:39:07 +0200

    a

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react'
-import '../styles/SecondPage.css'
+import React, { useState } from 'react';
+import '../styles/SecondPage.css';
 
 function SecondPage() {
 
-    let [blub, setBlub] = useState(0);
+    let [blub, setBlub] = useState(33329748040);
 
     function changeBlub (e) {
-        setBlub(e.target.value)
+        setBlub(e.target.value);
     }
 
-  return (
-    <div className='secondPageCont'>
-        <div className='secondPageHeaderCont'>
-            <img src="https://www.blubsui.com/t-blub.png" alt="blubsui" />
-            
-        </div>
-        <div className='calculateCont'>
-            <h1>In $BLUB, we always calculate billion-dollar conversions</h1>
-            <div className='calculateDiv'>
-                <small>Insert</small>
-                <input type="number" onChange={changeBlub}/>
-                <h2>$BLUB</h2>
+    function formatNumber(value) {
+        return (Math.floor(value / 2104) / 100).toLocaleString('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        });
+    }
+
+    return (
+        <div className='secondPageCont'>
+            <div className='secondPageHeaderCont'>
+                <img src="https://www.blubsui.com/t-blub.png" alt="blubsui" />
             </div>
-            <div className='resultDiv'>
-                <h2>Value in Dollars:</h2>
-                <p>${Math.floor(blub/2104) / 100}</p>
-                <h3>This is what you'll have when $BLUB
-                hits $1 BILLION. It's going to happen.</h3>
+            <div className='calculateCont'>
+                <h1>In $BLUB, we always calculate billion-dollar conversions</h1>
+                <div className='calculateDiv'>
+                    <small>Insert</small>
+                    <input type="number" onChange={changeBlub} />
+                    <h2>$BLUB</h2>
+                </div>
+                <div className='resultDiv'>
+                    <h2>Value in Dollars:</h2>
+                    <p>${formatNumber(blub)}</p>
+                    <h3>This is what you'll have when $BLUB hits $1 BILLION. It's going to happen.</h3>
+                </div>
             </div>
         </div>
-        
-    </div>
-  )
+    );
 }
 
-export default SecondPage
+export default SecondPage;
