145e13a6ba8045b3cc54c5f6315930e8798bb222
MilosGlamocak <milosglamocak@gmail.com>
2024-09-13 13:29:35 +0200

    fonts + styles

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -3,7 +3,12 @@ import '../styles/Tokenomics.css'
 
 function Tokenomics() {
   return (
-    <div className='tokenomicsCont'></div>
+    <div className='tokenomicsCont'>
+        <h2>$TOKENOMICS</h2>
+        <h3>TOKEN SUPPLY</h3>
+        <h4>420.690.000.000.000 $BLUB</h4>
+        <p>(420,690 T)</p>
+    </div>
   )
 }
 
