1e1df4a0f15fd08482c092db56938140a3d43d7d
MilosGlamocak <milosglamocak@gmail.com>
2024-09-13 10:49:43 +0200

    a

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -8,6 +8,11 @@ function SecondPage() {
             <img src="https://www.blubsui.com/t-blub.png" alt="blubsui" />
             <h1>In $BLUB, we always calculate billion-dollar conversions</h1>
         </div>
+        <div className='calculateDiv'>
+            <small>Insert</small>
+            <input type="number" />
+            <h2>$BLUB</h2>
+        </div>
     </div>
   )
 }
