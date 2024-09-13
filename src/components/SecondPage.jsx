0c973dae1f1c8f333ee6f855e4eb783185328cd9
MilosGlamocak <milosglamocak@gmail.com>
2024-09-13 11:29:32 +0200

    blub math

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/SecondPage.css'
 
 function SecondPage() {
+
+    let [blub, setBlub] = useState(0);
+
+    function changeBlub (e) {
+        setBlub(e.target.value)
+    }
+
   return (
     <div className='secondPageCont'>
         <div className='secondPageHeaderCont'>
@@ -12,12 +19,12 @@ function SecondPage() {
             <h1>In $BLUB, we always calculate billion-dollar conversions</h1>
             <div className='calculateDiv'>
                 <small>Insert</small>
-                <input type="number" />
+                <input type="number" onChange={changeBlub}/>
                 <h2>$BLUB</h2>
             </div>
             <div className='resultDiv'>
                 <h2>Value in Dollars:</h2>
-                <p>$43875</p>
+                <p>${Math.floor(blub/2104) / 100}</p>
                 <h3>This is what you'll have when $BLUB
                 hits $1 BILLION. It's going to happen.</h3>
             </div>
