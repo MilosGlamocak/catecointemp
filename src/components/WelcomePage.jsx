8d1c151b567534e231543ff163154652e728d7a2
MilosGlamocak <milosglamocak@gmail.com>
2024-09-14 13:20:37 +0200

    welcome styling

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,10 +1,13 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 import '../styles/WelcomePage.css';
 import MenuButton from './MenuButton';
 import XIcon from '@mui/icons-material/X';
 import TelegramIcon from '@mui/icons-material/Telegram';
 
 const WelcomePage = forwardRef(({ scrollToSection }, ref) => {
+
+  let [open, setOpen] = useState(false);
+
   const buttons = ['HOME', 'TOKENOMICS'];
 
   const handleScroll = (section) => {
@@ -17,12 +20,12 @@ const WelcomePage = forwardRef(({ scrollToSection }, ref) => {
         <img src="https://cateoneth.com/assets/img/logo.png" alt="logo" className='logoSmall'/>
         <div className='navBtnsDiv'>
           <a href="" className='headerBtn btn'>BUY CATE</a>
-          <button className='headerBurger btn'>
+          <button className={`headerBurger btn ${open ? 'headerOpen' : ''}`} onClick={() => setOpen(!open)}>
             <span></span>
             <span></span>
             <span></span>
           </button>
-          <nav className='headerNav btn'>
+          <nav className='headerNav btn' style={{top: open ? '6rem' : '-8.5rem', pointerEvents: open ? 'all' : 'none'}}>
             <a href="#home">Home</a>
             <a href="#tokenomics">Tokenomics</a>
             <a href="#how-to-buy">How to buy?</a>
