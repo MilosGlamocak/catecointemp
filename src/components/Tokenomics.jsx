d69c655ad1f53c547e01661d58b1996172c9ff4a
MilosGlamocak <milosglamocak@gmail.com>
2024-09-16 13:06:18 +0200

    tokenomics start

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -4,7 +4,9 @@ import '../styles/Tokenomics.css';
 const Tokenomics = forwardRef((props, ref) => {
   return (
     <div className='tokenomicsCont' ref={ref}>
-      
+      <div className='tokenomicsDiv'>
+        <h2 className='tokenomicsTitle'>TOKENOMICS</h2>
+      </div>
     </div>
   );
 });
